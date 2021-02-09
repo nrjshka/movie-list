0d680fa59b56b59adb4e6c4deeb04fb934287bfe
nrjshka <m.korolev@city-mobil.ru>
2021-02-10 01:04:20 +0300

    Added: definition of app component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React, { memo } from 'react'
+
+const App: React.FC = memo(function App() {
+  return <div>React App</div>
+})
+
+export { App }
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 
+import { App } from './components'
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>Hello</div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
