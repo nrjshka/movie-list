032e4375c39c29b196818efec01d5e9d9ce20e27
nrjshka <m.korolev@city-mobil.ru>
2021-02-10 01:34:00 +0300

    Added: antd global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
 
+import 'antd/dist/antd.css'
+
 import { App } from './components'
 
 ReactDOM.render(
