aeab83bff15711fcbeba9825fbe0950bef7c679d
nrjshka <m.korolev@city-mobil.ru>
2021-02-10 17:56:32 +0300

    Added: basic markup for header

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -1,9 +1,20 @@
 import React, { memo } from 'react'
 
-import { Layout } from 'antd'
+import { Content, Container, HeaderStyled, Section } from './Styled'
 
 const Header = memo(function Header() {
-  return <Layout.Header>Header</Layout.Header>
+  return (
+    <HeaderStyled>
+      <Container>
+        <Content>
+          <Section>Tranding</Section>
+          <Section>Watch Later</Section>
+          <Section>Favourite</Section>
+        </Content>
+        <Content>Search</Content>
+      </Container>
+    </HeaderStyled>
+  )
 })
 
 export { Header }
---
+++ src/ui/Header/Styled.tsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+import { Layout } from 'antd'
+
+const HeaderStyled = styled(Layout.Header)`
+  background: #fff;
+  height: auto;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+`
+
+const Content = styled.div`
+  display: flex;
+
+  flex-direction: row;
+
+  & > * {
+    margin-right: 10px;
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+`
+
+const Section = styled.div`
+  display: flex;
+`
+
+export { HeaderStyled, Content, Container, Section }
