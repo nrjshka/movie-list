68007bd034b1148476f628432aecd27100df6e6e
nrjshka <m.korolev@city-mobil.ru>
2021-02-10 17:28:09 +0300

    Added: ui packages; storybook

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React, { memo } from 'react'
 import { Layout } from 'antd'
 
+import { Header } from '../../ui'
+
 import { MainPage } from '..'
 
 const App: React.FC = memo(function App() {
   return (
     <Layout>
-      <Layout.Header>123</Layout.Header>
+      <Header />
       <Layout.Content>
         <MainPage />
       </Layout.Content>
---
+++ src/ui/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+import { Layout } from 'antd'
+
+const Header = memo(function Header() {
+  return <Layout.Header>Header</Layout.Header>
+})
+
+export { Header }
