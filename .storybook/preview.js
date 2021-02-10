23cb47908b7f87d332c019364cdc2624bf13e59e
nrjshka <m.korolev@city-mobil.ru>
2021-02-10 19:02:31 +0300

    Added: storybook theme provider setup

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,22 @@
+import React from 'react'
+import {  ThemeProvider } from 'styled-components';
+
+import { theme } from '../src/ui'
+
 import 'antd/dist/antd.css'
 
-export const parameters = {
+const withThemeProvider = (Story, context) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+
+const decorators = [withThemeProvider]
+
+const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+export { decorators, parameters }
