71a6f791556406f41a8cdf6b38a705050d098f26
nrjshka <m.korolev@city-mobil.ru>
2021-02-12 00:02:41 +0300

    Added: router history

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { ThemeProvider } from 'styled-components'
+import { Router } from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals'
 
@@ -8,12 +9,15 @@ import 'antd/dist/antd.css'
 
 import { App } from './components'
 import { theme } from './ui'
+import { appHistory } from './utils'
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
+    <Router history={appHistory}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 )
