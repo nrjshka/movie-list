d1926542041ff79c4de23e379298e52aa1f69b9e
nrjshka <m.korolev@city-mobil.ru>
2021-02-14 00:50:47 +0300

    Added: searching in watch later or in favourite

--- src/components/SavedMoviesList/SavedMoviesList.tsx
+++ src/components/SavedMoviesList/SavedMoviesList.tsx
@@ -4,16 +4,23 @@ import { useSelector } from 'react-redux'
 
 import { MovieList } from '..'
 
-import { SupportedStores, getSavedMovieData } from '../../redux'
+import { SupportedStores, getSavedMovieData, getSearchText } from '../../redux'
+
+const isSubString = (value: string, sub: string) => value.toLowerCase().indexOf(sub) >= 0
 
 const SavedMoviesList: React.FC<RouteComponentProps<{ category: SupportedStores }>> = memo(function SavedMoviesList(
   props,
 ) {
   const { category } = props.match.params
 
+  const searchText = useSelector(getSearchText)
   const data = useSelector(getSavedMovieData(category))
 
-  return <MovieList movies={data} />
+  const movies = data.filter(
+    ({ title, overview }) => isSubString(title, searchText) || isSubString(overview, searchText),
+  )
+
+  return <MovieList movies={movies} />
 })
 
 export { SavedMoviesList }
